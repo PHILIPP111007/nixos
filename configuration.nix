24b5e4e60143e02488449b45e054e162d46b91b8
Philipp Roshchin <r.phil@yandex.ru>
2025-09-02 20:03:19 +0300

    update

--- configuration.nix
+++ configuration.nix
@@ -5,10 +5,11 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./hyprland.nix
+    ./nvidia.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -45,74 +46,19 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  boot.kernelModules = [
-    # Базовые драйвера NVIDIA
-    "nvidia"
-    "nvidia_uvm"
-    "nvidia_drm"
-  ];
-
   # Enaable OpenGL
   hardware.graphics = {
-      enable = true;
+    enable = true;
   };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Hyprland
-  #programs.hyprland = {
-  #  enable = true;
-  #  withUWSM = true;
-  #  xwayland.enable = true;
-  #};
-
-  #environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  #environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
-
-  #programs.hyprlock.enable = true;
-  #services.hypridle.enable = true;
-  #programs.waybar.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
-  };
-
-  # NVIDIA
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Enable CUPS to print documents.
@@ -162,37 +108,36 @@
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
+  programs = {
+    firefox.enable = true;
 
-  # ZSH
-  programs.zsh = {
-    enable = true;
-    enableBashCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-
-    # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-    ohMyZsh = {
+    zsh = {
       enable = true;
-      plugins = [ "git" "history" ];
-      custom = "$HOME/.oh-my-zsh/custom/";
-      theme = "powerlevel10k/powerlevel10k";
+      enableBashCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+
+      # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+      ohMyZsh = {
+        enable = true;
+        plugins = [ "git" "history" ];
+        custom = "$HOME/.oh-my-zsh/custom/";
+        theme = "powerlevel10k/powerlevel10k";
+      };
+    };
+
+    git.config = {
+      init.defaultBranch = "main";
+      user = {
+        name = "Philipp Roshchin";
+        email = "r.phil@yandex.ru";
+      };
     };
   };
 
   system.userActivationScripts.zshrc = "touch .zshrc";
   environment.shells = with pkgs; [ zsh ];
   users.defaultUserShell = pkgs.zsh;
-
-  # GIT
-  programs.git.config = {
-    init.defaultBranch = "main";
-    user = {
-      name = "Philipp Roshchin";
-      email = "r.phil@yandex.ru";
-    };
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -204,6 +149,7 @@
     wget
     file
     tree
+    zip
     unzip
     gcc
 
@@ -215,47 +161,22 @@
     zsh
     zsh-powerlevel10k
 
-    # pyprland
-    # hyprpicker
-    # hyprcursor
-    # hyprlock
-    # hypridle
-    # hyprpaper
-    # hyprsunset
-    # hyprpolkitagent
-    # kitty
-    # waybar
-    # wofi
-    # dunst
+    hyprland
+    pyprland
+    hyprpicker
+    hyprcursor
+    hyprlock
+    hypridle
+    hyprpaper
+    hyprsunset
+    hyprpolkitagent
+    kitty
+    waybar
+    wofi
+    dunst
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
---
+++ hyprland.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  # Hyprland
+  programs.hyprland = {
+   enable = true;
+   withUWSM = true;
+   xwayland.enable = true;
+  };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+
+  programs.hyprlock.enable = true;
+  services.hypridle.enable = true;
+  programs.waybar.enable = true;
+}
