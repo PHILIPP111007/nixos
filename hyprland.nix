9ff881705975721c3af23d409ab8b25e35395cde
Philipp Roshchin <r.phil@yandex.ru>
2025-09-02 20:24:32 +0300

    update

--- hyprland.nix
+++ hyprland.nix
@@ -2,16 +2,16 @@
 
 {
   # Hyprland
-  programs.hyprland = {
-   enable = true;
-   withUWSM = true;
-   xwayland.enable = true;
-  };
+  # programs.hyprland = {
+  #  enable = tree;
+  #  withUWSM = true;
+  #  xwayland.enable = true;
+  # };
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
-  programs.hyprlock.enable = true;
-  services.hypridle.enable = true;
-  programs.waybar.enable = true;
+  # programs.hyprlock.enable = true;
+  # services.hypridle.enable = true;
+  # programs.waybar.enable = true;
 }
