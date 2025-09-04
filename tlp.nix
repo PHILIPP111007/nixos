1e74042983666e94ddd49ea429e72567826ed267
Philipp Roshchin <r.phil@yandex.ru>
2025-09-04 20:53:26 +0300

    update

--- tlp.nix
+++ tlp.nix
@@ -7,16 +7,23 @@
 
         settings = {
             CPU_SCALING_GOVERNOR_ON_AC = "performance";
-            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            CPU_SCALING_GOVERNOR_ON_BAT = "performance";
 
-            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+            CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+
+            CPU_BOOST_ON_AC=1;
+            CPU_BOOST_ON_BAT=0;
+
+            CPU_HWP_DYN_BOOST_ON_AC=1;
+            CPU_HWP_DYN_BOOST_ON_BAT=0;
 
             CPU_MIN_PERF_ON_AC = 0;
             CPU_MAX_PERF_ON_AC = 100;
 
             CPU_MIN_PERF_ON_BAT = 0;
-            CPU_MAX_PERF_ON_BAT = 20;
+            CPU_MAX_PERF_ON_BAT = 100;
         };
     };
 }
